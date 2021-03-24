77d6190a4f97d5da70fbc0610599fbec9be2dcb5
RafeStefano <stefanorafe@gmail.com>
2021-03-24 17:32:28 +0300

    added home_api app

--- src/clarusway/settings.py
+++ src/clarusway/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'fscohort'
+    'fscohort.apps.FscohortConfig',
+    'fscohort_api.apps.FscohortApiConfig',
 ]
 
 MIDDLEWARE = [
