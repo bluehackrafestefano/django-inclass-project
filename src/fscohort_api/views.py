725041067104fe6c785103355ebe76d9f4a689fb
RafeStefano <stefanorafe@gmail.com>
2021-03-24 19:04:20 +0300

    added serializer

--- src/fscohort_api/views.py
+++ src/fscohort_api/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from fscohort.models import Student
+from django.core.serializers import serialize
 
 def home_api(request):
     data = {
@@ -9,3 +11,32 @@ def home_api(request):
     }
     return JsonResponse(data)
 
+# def student_list_api(request):
+#     if request.method == "GET":
+#         students = Student.objects.all()
+#         student_count = Student.objects.count()
+        
+#         student_list = []
+#         for student in students:
+#             student_list.append({
+#                 "firstname": student.first_name,
+#                 "lastname": student.last_name,
+#                 "number": student.number,
+#             })
+        
+#         data = {
+#             "students": student_list,
+#             "count": student_count,
+#         }
+#         return JsonResponse(data)
+
+def student_list_api(request):
+    if request.method == "GET":
+        students = Student.objects.all()
+        student_count = Student.objects.count()
+        student_data = serialize("python", students)
+        data = {
+            "students": student_data,
+            "count": student_count,
+        }
+        return JsonResponse(data)
